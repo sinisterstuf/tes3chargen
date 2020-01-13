d1c662ea0a8779d8f461f15d3c6581c860524e82
Siôn le Roux <sinisterstuf@gmail.com>
2020-01-13 13:37:20 +0100

    Include Material UI and try out a custom styled button
    The button uses a theme I generated with the material design colour
    tool, I'm still not 100% certain these are the best colours, picking
    colours is hard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ffffe5',
+      main: '#ffecb3',
+      dark: '#cbba83',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#e6f9ff',
+      main: '#b3c6ff',
+      dark: '#8296cc',
+      contrastText: '#000',
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
@@ -10,14 +29,11 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ThemeProvider theme={theme}>
+          <Button variant="contained" color="primary">
+            Click me!
+          </Button>
+        </ThemeProvider>
       </header>
     </div>
   );
