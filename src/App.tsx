17ad6bd6f66d006dea20667ad70754418fc2a6bf
Siôn le Roux <sinisterstuf@gmail.com>
2020-01-19 01:01:46 +0100

    Replace example react stuff with a basic MUI form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Button,
+  RadioGroup,
+  Radio,
+  FormControl,
+  FormControlLabel,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Divider,
+  Box
+} from '@material-ui/core';
 
 const theme = createMuiTheme({
   palette: {
@@ -23,19 +34,29 @@ const theme = createMuiTheme({
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <ThemeProvider theme={theme}>
-          <Button variant="contained" color="primary">
-            Click me!
-          </Button>
-        </ThemeProvider>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box>
+            <Card>
+              <CardContent>
+                <Typography>
+                  On a clear day you chance upon a strange animal, its leg trapped in a hunter's clawsnare. Judging from the bleeding it will not survive long.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <FormControl>
+                  <RadioGroup>
+                    <FormControlLabel value="strength" control={<Radio />} label="Draw your dagger, mercifully ending its life with a single thrust." />
+                    <Divider />
+                    <FormControlLabel value="magic" control={<Radio />} label="Use herbs from your pack to put it to sleep." />
+                    <Divider />
+                    <FormControlLabel value="stealth" control={<Radio />} label="Do not interfere in the natural evolution of events, but rather take the opportunity to learn more about a strange animal you have never seen before." />
+                  </RadioGroup>
+                </FormControl>
+                <Button variant="contained">Next</Button>
+              </CardActions>
+            </Card>
+      </Box>
+    </ThemeProvider>
   );
 }
 
