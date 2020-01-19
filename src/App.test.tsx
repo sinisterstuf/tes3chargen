087d99daf36ed12711fd109dd16c91ae277d4960
Siôn le Roux <sinisterstuf@gmail.com>
2020-01-19 02:50:06 +0100

    Fix tests broken by content swap

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const questionElement = getByText(/On a clear day/i);
+  expect(questionElement).toBeInTheDocument();
+  const nextButton = getByText(/Next/i);
+  expect(nextButton).toBeInTheDocument();
 });
