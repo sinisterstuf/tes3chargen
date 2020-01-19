7b405d82dcc67428da31c29461341be013e63bd0
Siôn le Roux <sinisterstuf@gmail.com>
2020-01-19 02:40:36 +0100

    Make the first PoC question look nicer
    The first one had default styles, now I'm trying to tweak the styles a
    bit to add space and use theme colours.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ import {
   Typography,
   CardActions,
   Divider,
-  Box
+  Box,
+  Paper,
+  Container
 } from '@material-ui/core';
 
 const theme = createMuiTheme({
@@ -35,27 +37,25 @@ const theme = createMuiTheme({
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Box>
-            <Card>
-              <CardContent>
-                <Typography>
-                  On a clear day you chance upon a strange animal, its leg trapped in a hunter's clawsnare. Judging from the bleeding it will not survive long.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <FormControl>
-                  <RadioGroup>
-                    <FormControlLabel value="strength" control={<Radio />} label="Draw your dagger, mercifully ending its life with a single thrust." />
-                    <Divider />
-                    <FormControlLabel value="magic" control={<Radio />} label="Use herbs from your pack to put it to sleep." />
-                    <Divider />
-                    <FormControlLabel value="stealth" control={<Radio />} label="Do not interfere in the natural evolution of events, but rather take the opportunity to learn more about a strange animal you have never seen before." />
-                  </RadioGroup>
-                </FormControl>
-                <Button variant="contained">Next</Button>
-              </CardActions>
-            </Card>
-      </Box>
+      <Container className="main" maxWidth="md">
+        <Paper variant="outlined" className="question">
+          <Typography className="question-text" variant="h6">
+            On a clear day you chance upon a strange animal, its leg trapped in a hunter's clawsnare. Judging from the bleeding it will not survive long.
+          </Typography>
+          <FormControl component="fieldset">
+            <RadioGroup className="question-choices">
+              <FormControlLabel value="strength" control={<Radio />} label="Draw your dagger, mercifully ending its life with a single thrust." />
+              <Divider />
+              <FormControlLabel value="magic" control={<Radio />} label="Use herbs from your pack to put it to sleep." />
+              <Divider />
+              <FormControlLabel value="stealth" control={<Radio />} label="Do not interfere in the natural evolution of events, but rather take the opportunity to learn more about a strange animal you have never seen before." />
+            </RadioGroup>
+          </FormControl>
+          <Box display="flex" flexDirection="row-reverse">
+            <Button className="button">Next</Button>
+          </Box>
+        </Paper>
+      </Container>
     </ThemeProvider>
   );
 }
